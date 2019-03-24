1c6f1d970b512737c6870093ca4c1fe8664e8a61
Taylor Hopper <taylorhopper@outlook.com>
2019-03-24 16:34:17 -0700

    Set 16 bit registers

--- src/CPU.hs
+++ src/CPU.hs
@@ -1,8 +1,9 @@
 module CPU where
 
-    import Data.Word (Word8, Word16)
+    import Data.Bits
     import Data.Bool (Bool)
     import Data.IntMap (IntMap)
+    import Data.Word (Word8, Word16)
 
     data Registers = Registers {
         a :: Word8,
@@ -24,6 +25,9 @@ module CPU where
     pack :: Word8 -> Word8 -> Word16
     pack hi lo = fromIntegral hi * 256 + fromIntegral lo
 
+    unpack :: Word16 -> (Word8, Word8)
+    unpack val = (fromIntegral (shiftR val 8), fromIntegral val)
+
     toWord16 :: (Registers -> Word8) -> (Registers -> Word8) -> Registers -> Word16
     toWord16 hi lo registers = pack (hi registers) (lo registers) 
 
@@ -38,3 +42,19 @@ module CPU where
 
     hl :: Registers -> Word16
     hl = toWord16 h l
+
+    setAF :: Registers -> Word16 -> Registers
+    setAF registers val = let (hi, lo) = unpack val
+        in registers { a = hi, f = lo }
+
+    setBC :: Registers -> Word16 -> Registers
+    setBC registers val = let (hi, lo) = unpack val
+        in registers { b = hi, c = lo }
+
+    setDE :: Registers -> Word16 -> Registers
+    setDE registers val = let (hi, lo) = unpack val
+        in registers { d = hi, e = lo }
+
+    setHL :: Registers -> Word16 -> Registers
+    setHL registers val = let (hi, lo) = unpack val
+        in registers { h = hi, l = lo }
--- test/RegisterSpec.hs
+++ test/RegisterSpec.hs
@@ -18,14 +18,30 @@ module RegisterSpec where
 
     spec :: Spec
     spec = describe "register" $ do
-        it "Combines A and F for AF" $
+        it "Packs A and F into AF" $
             af (defaultRegister { a = 128, f = 128 }) `shouldBe` 32896
         
-        it "Combines B and C for BC" $
+        it "Packs B and C into BC" $
             bc (defaultRegister { b = 128, c = 128 }) `shouldBe` 32896
             
-        it "Combines D and E for DE" $
+        it "Packs D and E into DE" $
             de (defaultRegister { d = 128, e = 128 }) `shouldBe` 32896
 
-        it "Combines H and L for HL" $
-            bc (defaultRegister { b = 128, c = 128 }) `shouldBe` 32896
+        it "Packs H and L into HL" $
+            hl (defaultRegister { h = 128, l = 128 }) `shouldBe` 32896
+
+        it "Unpacks AF into A and F" $
+            let registers = setAF defaultRegister 32896
+            in (a registers, f registers) `shouldBe` (128, 128)
+        
+        it "Unpacks BC into B and C" $
+            let registers = setBC defaultRegister 32896
+            in (b registers, c registers) `shouldBe` (128, 128)
+            
+        it "Unpacks DE into D and E" $
+            let registers = setDE defaultRegister 32896
+            in (d registers, e registers) `shouldBe` (128, 128)
+
+        it "Unpacks HL into H and L" $
+            let registers = setHL defaultRegister 32896
+            in (h registers, l registers) `shouldBe` (128, 128)
