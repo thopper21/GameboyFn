b0e73a078f4a517d7faaa540a6d6b14316111940
Taylor Hopper <taylorhopper@outlook.com>
2019-04-07 12:40:10 -0700

    Add memory interface

--- src/CPU.hs
+++ src/CPU.hs
@@ -2,10 +2,8 @@ module CPU where
 
 import           Data.IntMap (IntMap)
 import           Data.Word   (Word8)
+import           Memory      (Memory)
 import           Register    (Registers)
-
-newtype Memory =
-  IntMap Word8
 
 data CPU = CPU
   { registers :: Registers
