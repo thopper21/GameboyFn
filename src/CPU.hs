73b07496772db3530264180691e29bc88033c483
Taylor Hopper <taylorhopper@outlook.com>
2019-04-07 15:16:51 -0700

    Add emptyCPU function

--- src/CPU.hs
+++ src/CPU.hs
@@ -2,10 +2,13 @@ module CPU where
 
 import           Data.IntMap (IntMap)
 import           Data.Word   (Word8)
-import           Memory      (Memory)
-import           Register    (Registers)
+import           Memory      (Memory, emptyMemory)
+import           Register    (Registers, emptyRegisters)
 
 data CPU = CPU
   { registers :: Registers
   , memory    :: Memory
   }
+
+emptyCPU :: CPU
+emptyCPU = CPU {registers = emptyRegisters, memory = emptyMemory}
