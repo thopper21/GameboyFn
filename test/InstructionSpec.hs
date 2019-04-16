2e57a5eaa4900c4f8103bab79067a32fa8daf890
Taylor Hopper <taylorhopper@outlook.com>
2019-04-15 19:34:03 -0700

    Refactor instruction test

--- test/InstructionSpec.hs
+++ test/InstructionSpec.hs
@@ -25,16 +25,14 @@ spec = do
             (cpu', time) = instruction cpu
          in time `shouldBe` 12
       it "Writes to BC" $
-        let memory = write8 0 0x01 emptyMemory
-            memory' = write16 1 42 memory
-            cpu = CPU {memory = memory', registers = emptyRegisters}
+        let memory = write8 0 0x01 . write16 1 42 $ emptyMemory
+            cpu = CPU {memory = memory, registers = emptyRegisters}
             (cpu', time) = instruction cpu
             reg = registers cpu'
          in getRegister16 BC reg `shouldBe` 42
       it "Includes both hi and lo bytes" $
-        let memory = write8 0 0x01 emptyMemory
-            memory' = write16 1 4242 memory
-            cpu = CPU {memory = memory', registers = emptyRegisters}
+        let memory = write8 0 0x01 . write16 1 4242 $ emptyMemory
+            cpu = CPU {memory = memory, registers = emptyRegisters}
             (cpu', time) = instruction cpu
             reg = registers cpu'
          in getRegister16 BC reg `shouldBe` 4242
