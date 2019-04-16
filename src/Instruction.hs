fb915b35237558e165445570d447092d39c88a55
Taylor Hopper <taylorhopper@outlook.com>
2019-04-15 21:03:05 -0700

    Implement LD (BC),nn

--- src/Instruction.hs
+++ src/Instruction.hs
@@ -4,8 +4,9 @@ module Instruction
 
 import           CPU       (CPU (..))
 import           Data.Word (Word16, Word8)
-import           Memory    (Memory, read16, read8)
-import           Register  (Register16 (..), getRegister16, setRegister16)
+import           Memory    (Memory, read16, read8, write8)
+import           Register  (Register16 (..), Register8 (..), getRegister16,
+                            getRegister8, setRegister16)
 
 nop :: CPU -> (CPU, Int)
 nop cpu = (cpu, 0)
@@ -24,10 +25,17 @@ readImmediate8 = readImmediate read8 1
 readImmediate16 :: CPU -> (CPU, Word16, Int)
 readImmediate16 = readImmediate read16 2
 
+readRegister8 :: Register8 -> CPU -> (Word8, Int)
+readRegister8 register cpu = (getRegister8 register . registers $ cpu, 0)
+
 writeReg16 :: Register16 -> CPU -> Word16 -> (CPU, Int)
 writeReg16 register cpu value =
   let registers' = setRegister16 register value (registers cpu)
    in (cpu {registers = registers'}, 0)
+
+writeAddress8 :: Word16 -> CPU -> Word8 -> (CPU, Int)
+writeAddress8 address cpu value =
+  (cpu {memory = write8 address value . memory $ cpu}, 4)
 
 readConstant16 :: Word16 -> Int -> CPU -> (Word16, Int)
 readConstant16 value time _ = (value, time)
@@ -45,6 +53,9 @@ getOperation op cpu =
     0x01 ->
       let (cpu', value, readTime) = readImmediate16 cpu
        in ld (writeReg16 BC) (readConstant16 value readTime) cpu'
+    0x02 ->
+      let address = getRegister16 BC . registers $ cpu
+       in ld (writeAddress8 address) (readRegister8 A) cpu
 
 instruction :: CPU -> (CPU, Int)
 instruction cpu =
--- test/InstructionSpec.hs
+++ test/InstructionSpec.hs
@@ -9,18 +9,21 @@ import           Test.Hspec
 fromMemory :: Memory -> CPU
 fromMemory memory = CPU {memory = memory, registers = emptyRegisters}
 
+fromRegisters :: Registers -> CPU
+fromRegisters registers = CPU {memory = emptyMemory, registers = registers}
+
 incrementsPC op amount =
   it ("Increments PC by " ++ show amount) $
-    let memory = write8 0 op emptyMemory
-        (cpu, _) = instruction $ fromMemory memory
-        value = getRegister16 PC . registers $ cpu
-    in value `shouldBe` amount
+  let memory = write8 0 op emptyMemory
+      (cpu, _) = instruction $ fromMemory memory
+      value = getRegister16 PC . registers $ cpu
+   in value `shouldBe` amount
 
-costsCycles op cycles = 
+costsCycles op cycles =
   it ("Costs " ++ show cycles ++ " cycles") $
-    let memory = write8 0 op emptyMemory
-        (_, time) = instruction $ fromMemory memory
-    in time `shouldBe` cycles
+  let memory = write8 0 op emptyMemory
+      (_, time) = instruction $ fromMemory memory
+   in time `shouldBe` cycles
 
 spec :: Spec
 spec = do
@@ -28,7 +31,7 @@ spec = do
     describe "NOP (0x00)" $ do
       0x00 `incrementsPC` 1
       0x00 `costsCycles` 4
-  describe "LD" $
+  describe "LD" $ do
     describe "BC,nn (0x01)" $ do
       0x01 `incrementsPC` 3
       0x01 `costsCycles` 12
@@ -42,3 +45,12 @@ spec = do
             (cpu, _) = instruction $ fromMemory memory
             result = getRegister16 BC . registers $ cpu
          in result `shouldBe` 4242
+    describe "(BC),nn (0x02)" $ do
+      0x02 `incrementsPC` 1
+      0x02 `costsCycles` 8
+      it "Writes A to (BC)" $
+        let reg = setRegister8 A 42 . setRegister16 BC 123 $ emptyRegisters
+            mem = write8 0 0x02 emptyMemory
+            (cpu, _) = instruction $ CPU {memory = mem, registers = reg}
+            value = read8 123 . memory $ cpu
+         in value `shouldBe` 42
