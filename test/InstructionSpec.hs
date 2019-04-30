337a34a6a1263d77c7e61fdeac1500ded66b51d7
Taylor Hopper <taylorhopper@outlook.com>
2019-04-29 19:42:03 -0700

    Reformat instruction spec

--- test/InstructionSpec.hs
+++ test/InstructionSpec.hs
@@ -27,31 +27,31 @@ costsCycles op cycles =
    in time cpu `shouldBe` cycles
 
 spec :: Spec
-spec = do
-  describe "Instruction" $
+spec =
+  describe "Instruction" $ do
     describe "NOP (0x00)" $ do
       0x00 `incrementsPC` 1
       0x00 `costsCycles` 4
-  describe "LD" $ do
-    describe "BC,nn (0x01)" $ do
-      0x01 `incrementsPC` 3
-      0x01 `costsCycles` 12
-      it "Writes to BC" $
-        let memory = write8 0 0x01 . write16 1 42 $ emptyMemory
-            cpu = instruction $ fromMemory memory
-            result = getRegister16 BC . registers $ cpu
-         in result `shouldBe` 42
-      it "Includes both hi and lo bytes" $
-        let memory = write8 0 0x01 . write16 1 4242 $ emptyMemory
-            cpu = instruction $ fromMemory memory
-            result = getRegister16 BC . registers $ cpu
-         in result `shouldBe` 4242
-    describe "(BC),nn (0x02)" $ do
-      0x02 `incrementsPC` 1
-      0x02 `costsCycles` 8
-      it "Writes A to (BC)" $
-        let reg = setRegister8 A 42 . setRegister16 BC 123 $ emptyRegisters
-            mem = write8 0 0x02 emptyMemory
-            cpu = instruction $ CPU {memory = mem, registers = reg, time = 0}
-            value = read8 123 . memory $ cpu
-         in value `shouldBe` 42
+    describe "LD" $ do
+      describe "BC,nn (0x01)" $ do
+        0x01 `incrementsPC` 3
+        0x01 `costsCycles` 12
+        it "Writes to BC" $
+          let memory = write8 0 0x01 . write16 1 42 $ emptyMemory
+              cpu = instruction $ fromMemory memory
+              result = getRegister16 BC . registers $ cpu
+           in result `shouldBe` 42
+        it "Includes both hi and lo bytes" $
+          let memory = write8 0 0x01 . write16 1 4242 $ emptyMemory
+              cpu = instruction $ fromMemory memory
+              result = getRegister16 BC . registers $ cpu
+           in result `shouldBe` 4242
+      describe "(BC),nn (0x02)" $ do
+        0x02 `incrementsPC` 1
+        0x02 `costsCycles` 8
+        it "Writes A to (BC)" $
+          let reg = setRegister8 A 42 . setRegister16 BC 123 $ emptyRegisters
+              mem = write8 0 0x02 emptyMemory
+              cpu = instruction $ CPU {memory = mem, registers = reg, time = 0}
+              value = read8 123 . memory $ cpu
+           in value `shouldBe` 42
